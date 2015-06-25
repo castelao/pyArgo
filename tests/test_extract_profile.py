cd4c3a994d4cc943d528c20ceb8659b350ff1250
Gui <guilherme@castelao.net>
2015-06-25 11:01:37 -0700

    Specific import of extract_profile.

--- tests/test_extract_profile.py
+++ tests/test_extract_profile.py
@@ -5,22 +5,22 @@
 
 """
 
-from argo import argo
+from argo.argo import extract_profile
 
 def validate_profile(p):
     pass
 
 def test_extract_1_profile():
-    p = argo.extract_profile('test_data/D20150316_prof.nc', 0)
+    p = extract_profile('test_data/D20150316_prof.nc', 0)
     assert len(p) == 1
     validate_profile(p)
 
 def test_extract_list_profiles():
-    p = argo.extract_profile('test_data/D20150316_prof.nc', [0, 2, 5])
+    p = extract_profile('test_data/D20150316_prof.nc', [0, 2, 5])
     assert len(p) == 3
     validate_profile(p)
 
 def test_extract_all_profiles():
-    p = argo.extract_profile('test_data/D20150316_prof.nc')
+    p = extract_profile('test_data/D20150316_prof.nc')
     assert len(p) > 1
     validate_profile(p)
