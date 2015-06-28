d5b187aa0b840ad5e8c8b5819812ad09cc7542f7
Gui <guilherme@castelao.net>
2015-06-28 13:20:00 -0700

    PEP8

--- tests/test_profile_from_nc.py
+++ tests/test_profile_from_nc.py
@@ -7,10 +7,12 @@
 
 from argo.argo import profile_from_nc
 
+
 def validate_profile(p):
     assert hasattr(p, 'keys')
     assert hasattr(p, 'attributes')
     assert 'datetime' in p.attributes
+
 
 def test_extract_1_profile():
     pp = profile_from_nc('test_data/20150127_prof.nc', 0)
@@ -18,11 +20,13 @@ def test_extract_1_profile():
     for p in pp:
         validate_profile(p)
 
+
 def test_extract_list_profiles():
     pp = profile_from_nc('test_data/20150127_prof.nc', [0, 2, 5])
     assert len(pp) == 3
     for p in pp:
         validate_profile(p)
+
 
 def test_extract_all_profiles():
     pp = profile_from_nc('test_data/20150127_prof.nc')
